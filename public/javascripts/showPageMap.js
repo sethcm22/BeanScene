af109d9aa7b97eaf005b4aecde07644dcfbae2f0
Coty Seth <sethcm22@gmail.com>
2023-10-11 17:19:21 -0400

    Added index clustermap

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,8 +1,26 @@
 mapboxgl.accessToken = mbxToken;
+
+// const popup = new mapboxgl.Popup({
+//   offset: popupOffsets,
+//   className: "my-class",
+// })
+//   .setHTML("<h1>Hello World!</h1>")
+//   .setMaxWidth("300px");
+
 const map = new mapboxgl.Map({
   container: "map", // container ID
   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
   style: "mapbox://styles/mapbox/streets-v11", // style URL
-  center: [-98.7781, 38.5181], // starting position [lng, lat]
-  zoom: 3, // starting zoom
+  center: coffeeshop.geometry.coordinates, // starting position [lng, lat]
+  zoom: 9, // starting zoom
 });
+
+// Set marker options.
+const marker = new mapboxgl.Marker()
+  .setLngLat(coffeeshop.geometry.coordinates)
+  .addTo(map)
+  .setPopup(
+    new mapboxgl.Popup().setHTML(
+      `<h3>${coffeeshop.name}</h1><p>${coffeeshop.location}</p>`
+    )
+  );
