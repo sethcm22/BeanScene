f92649cac73bd1e33de7aa117edebfa047600beb
Coty Seth <sethcm22@gmail.com>
2023-10-12 15:21:50 -0400

    config and use helmet

--- app.js
+++ app.js
@@ -45,11 +45,6 @@ app.use(mongoSanitize());
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
 app.use(morgan("dev"));
-app.use(
-  helmet({
-    contentSecurityPolicy: false,
-  })
-);
 
 //      Express Session
 const sessionConfig = {
@@ -82,6 +77,64 @@ app.use((req, res, next) => {
   res.locals.currentUser = req.user;
   next();
 });
+
+//      Helmet Config
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net/",
+  `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`,
+];
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net/",
+  `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`,
+];
+const connectSrcUrls = [
+  "https://*.tiles.mapbox.com",
+  "https://api.mapbox.com",
+  "https://events.mapbox.com",
+  `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`,
+];
+const fontSrcUrls = [
+  `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`,
+];
+
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: [],
+        connectSrc: ["'self'", ...connectSrcUrls],
+        scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+        styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+        workerSrc: ["'self'", "blob:"],
+        objectSrc: [],
+        imgSrc: [
+          "'self'",
+          "blob:",
+          "data:",
+          `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`, //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+          "https://images.unsplash.com/",
+        ],
+        fontSrc: ["'self'", ...fontSrcUrls],
+        mediaSrc: [
+          `https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`,
+        ],
+        childSrc: ["blob:"],
+      },
+    },
+    crossOriginEmbedderPolicy: false,
+  })
+);
 
 app.use("/coffeeshops", coffeeshopRoutes);
 app.use("/", userRoutes);
